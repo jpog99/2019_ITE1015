a0ab6df2ab2f3af6f29bbbdb2ef1d178e58b13c4
Wan <aus.baik@gmail.com>
2019-10-29 00:16:59 +0900

    Update setfunc.h

--- 7-2-2/setfunc.h
+++ 7-2-2/setfunc.h
@@ -3,7 +3,7 @@
 #include <set>
 using namespace std;
 
-set<int> parseSet(string& str);
+set<int> parseSet(const string& str);
 void printSet(const set<int>&);
 set<int> getIntersection(const set<int>& set0, const set<int>& set1);
 set<int> getUnion(const set<int>& set0, const set<int>& set1);
